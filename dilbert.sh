f9444602d6c10e2aac049fe6fcb36e17c81ba23d
Ondra Gersl <ondra.gersl@gmail.com>
2012-11-06 09:01:00 +0100

    Vylepseni kodu

--- dilbert.sh
+++ dilbert.sh
@@ -2,12 +2,13 @@
 
 date_start='1989-04-16'
 date_end=$(date +"%F")  # today
+url='http://www.dilbert.com'
 
 i=0
 while [ "$date_current" != "$date_end" ]; do
 	date_current=$(date --date="$(date --date=${date_start} +%F) + $i day" +"%F")
-	link=$(wget -q -O - http://www.dilbert.com/${date_current}/ | grep '.strip.zoom.gif' | sed 's/^\(.*\) src="//;s/" title=\(.*\)$//;')
-	link="http://www.dilbert.com${link}"
+	link=$(wget -q -O - ${url}/${date_current}/ | grep '.strip.zoom.gif' | sed 's/^\(.*\) src="//;s/" title=\(.*\)$//;')
+	link="${url}${link}"
 	wget -nv -O "${date_current}.gif" "$link"
 	i=$(expr $i + 1)
 done

e6e25905eafd5cd13d0a5a47e90d21d1df87286a
Ondra Gersl <ondra.gersl@gmail.com>
2012-11-07 12:07:12 +0100

    Stazeni pouze dnesniho komiksu

--- dilbert.sh
+++ dilbert.sh
@@ -4,6 +4,10 @@ date_start='1989-04-16'
 date_end=$(date +"%F")  # today
 url='http://www.dilbert.com'
 
+if [ "$1" = 'today' ]; then
+	date_start=$(date +"%F")  # today
+fi
+
 i=0
 while [ "$date_current" != "$date_end" ]; do
 	date_current=$(date --date="$(date --date=${date_start} +%F) + $i day" +"%F")
